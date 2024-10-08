407257b202e5cbab963bed79062796615c4e0de0
jasoncUD <jasonc@udel.edu>
2024-10-08 17:26:10 -0400

    Added backend springboot

---
+++ backend_project1/src/main/java/backend_project1/backend_project1/BackendProject1Application.java
@@ -0,0 +1,13 @@
+package backend_project1.backend_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProject1Application.class, args);
+	}
+
+}
---
+++ backend_project1/src/test/java/backend_project1/backend_project1/BackendProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package backend_project1.backend_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
